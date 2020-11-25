a2d3975a629ef1d39f92f87c905f94fd1dd4fcff
Milutin Spaic <milutin.spaic@bankom.rs>
2020-11-25 10:52:40 +0100

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
 using Core.Entities;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,17 +14,34 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        
+        private readonly IProductRepository repo;
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            this.repo = repo;
+           
+        }
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands () 
+        {
+
+            var brands = await repo.GetProductBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType () 
+        {
+
+            var types= await repo.GetProductTypesAsync();
+            return Ok(types);
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
 
-            var products =await  _context.Products.ToListAsync();
+            var products = await repo.GetProductsAsync();
             return Ok(products);
 
         }
@@ -31,7 +49,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-           return await _context.Products.FindAsync(id);
+            return await repo.GetProductByIdAsync(id);
 
 
         }
