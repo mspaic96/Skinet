f9b3e949c653e3f9dbf9fe5b0dc2a846e7191456
Milutin Spaic <milutin.spaic@bankom.rs>
2020-11-27 09:44:57 +0100

    End of section five

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,12 +11,13 @@ using Core.Specifications;
 using API.Dtos;
 using System.Linq;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    
+    public class ProductsController : BaseApiController
     {
 
         private readonly IGenericRepository<Product> productsRepo;
@@ -42,6 +43,8 @@ namespace API.Controllers
             return Ok(await productBrandsRepo.ListAllSync());
 
         }
+        
+       
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType()
@@ -61,12 +64,16 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
 
             var product = await productsRepo.GetEntityWithSpec(spec);
+
+            if(product == null)  return NotFound(new ApiResponse(404));
 
             return mapper.Map<Product,ProductToReturnDto>(product);
 
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Infrastructure.Data;
 
 using Microsoft.EntityFrameworkCore;
-using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.Middleware;
+using API.Extensions;
 
 namespace API
 {
@@ -33,27 +26,32 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
+            
             services.AddControllers();
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddDbContext<StoreContext> (x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+           services.AddApplicationServices();
+
+           services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+            
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumetation();
 
             app.UseEndpoints(endpoints =>
             {
